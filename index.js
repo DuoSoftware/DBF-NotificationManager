9ac9e099556009f06b7556d591564757089ec37e
KalanaDemel2 <kalana@duosoftware.com>
2018-12-06 10:44:14 +0530

    Bug Fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
             let value = await UserNotification.sendNotification(companyname, payload, token);
             return ({IsSuccess : true, message :`Success`, value :value });
         } catch(err) {
-            return({IsSuccess : false, message :`Date-${Date.now()}-redis set failed key-${key}`, exception :err });
+            return({IsSuccess : false, message :`sendNotification Failed`, exception :err });
         }
     }
 
